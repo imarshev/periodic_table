c5ae71ba00146a9adc0507dacd2af7f4c3c3acaf
imarshev <imarshev@mail.ru>
2022-03-27 13:11:33 +0300

    upd.

--- lib/table.rb
+++ lib/table.rb
@@ -1,8 +1,19 @@
+require_relative "element"
+
 class Table
   def initialize(elements)
     @elements = elements
     @table = Array.new(10) {Array.new(18)}
     elements.each { |element| @table[element.ypos - 1][element.xpos - 1] = element.symbol}
+  end
+
+  def about_element(symbol)
+    chosed_element = @elements.select { |element| element.symbol.downcase == symbol.downcase}[0]
+    if !chosed_element.nil?
+      chosed_element.to_s
+    else
+      "Элемента с таким символом нет в таблице. Ты открыл новый или просто опечатался?"
+    end
   end
 
   def to_s
@@ -19,14 +30,5 @@ class Table
         end
       end.join(" ")
     end.join("\n")
-  end
-
-  def about_element(symbol)
-    chosed_element = @elements.select { |element| element.symbol.downcase == symbol.downcase}[0]
-    if !chosed_element.nil?
-      chosed_element.to_s
-    else
-      "Элемента с таким символом нет в таблице. Ты открыл новый или просто опечатался?"
-    end
   end
 end
